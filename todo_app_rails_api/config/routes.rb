d1917dace49e29f0268bb64ae54a04a66695bd21
Francisco Chacon <pacochacon94@gmail.com>
2018-10-11 22:23:24 -0500

    Update routes to generate Api::V1 namespace set root path for Api::V1 namespace -> todos#index and root path of application -> api/v1/todos#index

--- todo_app_rails_api/config/routes.rb
+++ todo_app_rails_api/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :todos
+  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :todos
+      resources :users
+      root 'todos#index'
+    end
+  end
+
+  root 'api/v1/todos#index'
 end
