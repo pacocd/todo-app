fe15e72b5f2bd5f927ce40dca5d46026a780f55d
Francisco Chacon <pacochacon94@gmail.com>
2018-10-14 00:58:17 -0500

    Fetch todo list data when TodoList screen will be mounted

--- TodoAppRN/src/screens/TodoList.js
+++ TodoAppRN/src/screens/TodoList.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import APIManager from '../managers/APIManager';
+import APIErrorManager from '../managers/APIErrorManager';
+import { setTodoListData } from '../redux/actions';
 
 export class TodoList extends Component {
   static propTypes = {
-    navigation: PropTypes.object.isRequired
+    navigation: PropTypes.object.isRequired,
+    getTodoList: PropTypes.func,
+    hasError: PropTypes.func
   };
+
+  static defaultProps = {
+    getTodoList: APIManager.getTodoList,
+    hasError: APIErrorManager.hasError
+  };
+
   static navigationOptions = { title: 'Todo List' };
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const todoListData = await this.props.getTodoList();
+    const hasError = this.props.hasError(todoListData);
+
+    if (hasError) {
+      // Pending error handling
+    } else {
+      this.props.setTodoListData(todoListData.data);
+    }
+  }
 
   render() {
     return <View style={{ backgroundColor: 'white', flex: 1 }} />;
   }
 }
 
-export default TodoList;
+const mapStateToProps = ({ todos }) => {
+  const { todoListData } = todos;
+  return { todoListData };
+};
+
+const mapDispatchToProps = dispatch => {
+  return { setTodoListData: data => dispatch(setTodoListData(data)) };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
--- TodoAppRN/tests/screens/TodoList.test.js
+++ TodoAppRN/tests/screens/TodoList.test.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import TodoList from '../../src/screens/TodoList';
+import { TodoList } from '../../src/screens/TodoList';
 
 configure({ adapter: new Adapter() });
 
-describe('when TodoList is full rendered', () => {
-  const props = { navigation: {} };
+describe('when TodoList is fully rendered', () => {
+  const props = {
+    navigation: {},
+    getTodoList: jest
+      .fn()
+      .mockReturnValue(
+        new Promise(resolve => resolve({ data: { status: 200 } }))
+      ),
+    hasError: jest.fn().mockReturnValue(false),
+    setTodoListData: jest.fn()
+  };
   const wrapper = shallow(<TodoList {...props} />);
+
+  beforeAll(async () => {
+    await wrapper.instance().props.getTodoList();
+  });
 
   it('renders correctly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('expects getTodoList prop function to have been called', () => {
+    expect(wrapper.instance().props.getTodoList).toHaveBeenCalled();
+  });
+
+  it('returns an error in getTodoList response', () => {
+    expect(wrapper.instance().props.hasError).toHaveBeenCalledWith({
+      data: {
+        status: 200
+      }
+    });
+  });
+
+  it('expects setTodoListData to have been called with data from getTodoList', () => {
+    expect(wrapper.instance().props.setTodoListData).toHaveBeenCalledWith({
+      status: 200
+    });
   });
 });
