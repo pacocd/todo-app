46913902bcc99882801390155ef403f3cd6a6354
Francisco Chacon <pacochacon94@gmail.com>
2018-10-14 20:48:54 -0500

    Generate redux implementation to manipulate editingFieldsData This is going to set and reset editing fields data

--- TodoAppRN/src/redux/reducers/todo_reducer.js
+++ TodoAppRN/src/redux/reducers/todo_reducer.js
@@ -1,6 +1,10 @@
-import { SET_TODO_LIST_DATA } from '../actions/types';
+import {
+  SET_TODO_LIST_DATA,
+  RESET_EDITING_FIELDS_DATA,
+  SET_EDITING_FIELDS_DATA
+} from '../actions/types';
 
-const initialState = { todoListData: undefined };
+const initialState = { todoListData: undefined, editingFieldsData: {} };
 
 export default function(state = initialState, action) {
   switch (action.type) {
@@ -8,6 +12,19 @@ export default function(state = initialState, action) {
       const { todoListData } = action;
       return { ...state, todoListData };
     }
+    case SET_EDITING_FIELDS_DATA:
+      const {
+        editingFieldsData: { fieldName, value }
+      } = action;
+      return {
+        ...state,
+        editingFieldsData: {
+          ...state.editingFieldsData,
+          [`${fieldName}`]: value
+        }
+      };
+    case RESET_EDITING_FIELDS_DATA:
+      return { ...state, editingFieldsData: {} };
     default:
       return state;
   }
--- TodoAppRN/tests/redux/todo_actions.test.js
+++ TodoAppRN/tests/redux/todo_actions.test.js
@@ -1,10 +1,27 @@
-import { setTodoListData } from '../../src/redux/actions';
+import {
+  setTodoListData,
+  setEditingFieldsData,
+  resetEditingFieldsData
+} from '../../src/redux/actions';
 
 describe('when todo actions are called', () => {
   it('calls setTodoListData to get correct type and todoListData', () => {
     expect(setTodoListData({})).toEqual({
       type: 'SET_TODO_LIST_DATA',
       todoListData: {}
+    });
+  });
+
+  it('calls setEditingFieldsData to update edited fields', () => {
+    expect(setEditingFieldsData('test', 12)).toEqual({
+      type: 'SET_EDITING_FIELDS_DATA',
+      editingFieldsData: { fieldName: 'test', value: 12 }
+    });
+  });
+
+  it('calls resetEditingFieldsData to reset stored data from todos editing fields', () => {
+    expect(resetEditingFieldsData()).toEqual({
+      type: 'RESET_EDITING_FIELDS_DATA'
     });
   });
 });
